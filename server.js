831e9e3105ae6373e111a2027aa0e2eaf49445a8
Abhishek <anand2000ys@gmail.com>
2023-06-18 00:25:31 +0530

    custom middleware via error-handling added

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/contact", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
