41a4add936b76fbcd09f697f49fbd27df3aa0e5f
Abhishek <anand2000ys@gmail.com>
2023-06-19 21:32:09 +0530

    logic added for user registration

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,12 +1,39 @@
-const express = require("express");
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register user
 //@route POST /api/user/register
 //@access Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register the user" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill all the fields");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable && userAvailable.email === email) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hashing the password
+  //   const hashedPassword = await bcrypt.hash(password, 10);
+  //   console.log(hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password /*: hashedPassword*/,
+  });
+  console.log(`User created with ${user}`);
+  if (user) {
+    res.status(201).jsin({ _is: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+  res.json();
 });
 
 //@desc Loginn user
